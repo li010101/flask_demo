b91587cc503b81b74b6b1a923aee83a0caae9365
li010101 <liyaohui.123@163.com>
2020-11-17 10:22:52 +0800

    重新使用sqlite数据库

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,18 +7,24 @@
 """
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app(test_config=None):
     # 创建应用实例
-    app = Flask(__name__,instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     # 进行初始配置
     app.config.from_mapping(
-        SECKET_KEY = 'dev',
-        DATABASE = os.path.join(app.instance_path,'flaskr.sqlite')
+        SECKET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/flask_demo'
+    # 创建数据库对象
+    # db = SQLAlchemy(app)
+
     # 加载默认配置文件
     if test_config is None:
-        app.config.from_pyfile('config.py',silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         # 用于测试
         app.config.from_mapping(test_config)
@@ -30,12 +36,14 @@ def create_app(test_config=None):
 
     @app.route('/hello')
     def hello():
-        return  "hello world"
+        return "hello world"
+
+    # 注册数据库
+    from . import db
+    db.init_app(app)
 
     # 注册视图
     from . import auth
     app.register_blueprint(auth.bp)
 
     return app
-
-
